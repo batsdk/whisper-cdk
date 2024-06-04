74a74d73fb02447a430e2693bc6f4ee440b39968
shemil <shemilkaweesha@gmail.com>
2024-06-04 20:29:56 +0530

    Registering the Lambda

--- whisper-cdk.go
+++ whisper-cdk.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"github.com/aws/aws-cdk-go/awscdk/v2"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awslambda"
+
 	// "github.com/aws/aws-cdk-go/awscdk/v2/awssqs"
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
@@ -18,12 +20,21 @@ func NewWhisperCdkStack(scope constructs.Construct, id string, props *WhisperCdk
 	}
 	stack := awscdk.NewStack(scope, &id, &sprops)
 
-	// The code that defines your stack goes here
+	awslambda.NewFunction(stack, jsii.String("WhisperCdkFunction"), &awslambda.FunctionProps{
+		Runtime: awslambda.Runtime_PROVIDED_AL2023(),
+		Code:    awslambda.AssetCode_FromAsset(jsii.String("lambda/function.zip"), nil),
+		Handler: jsii.String("main"),
+	})
 
-	// example resource
-	// queue := awssqs.NewQueue(stack, jsii.String("WhisperCdkQueue"), &awssqs.QueueProps{
-	// 	VisibilityTimeout: awscdk.Duration_Seconds(jsii.Number(300)),
-	// })
+	//api := awsapigateway.NewRestApi(stack, jsii.String("whisperApi"), &awsapigateway.RestApiProps{
+	//	DefaultCorsPreflightOptions: &awsapigateway.CorsOptions{
+	//		AllowHeaders: jsii.Strings("Content-Type", "Authorization"),
+	//		AllowMethods: jsii.Strings("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"),
+	//		AllowOrigins: jsii.Strings("*"),
+	//	},
+	//})
+	//
+	//integration := awsapigateway.NewLambdaIntegration(lambdaFunc, nil)
 
 	return stack
 }
