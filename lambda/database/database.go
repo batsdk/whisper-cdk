c2bb2fdb7076048529052bc1d2c177fb3dfabbf1
shemil <shemilkaweesha@gmail.com>
2024-06-08 00:27:37 +0530

    added new database logic

--- lambda/database/database.go
+++ lambda/database/database.go
@@ -1,15 +1,43 @@
 package database
 
 import (
+	"whisper-lambda/types"
+
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
-type IDatabase interface {
-}
+const (
+	TABLE_NAME = "convoGroups"
+)
 
 type DynamoDBClient struct {
 	databaseStore *dynamodb.DynamoDB
+}
+
+func (d DynamoDBClient) CreateGroup(group types.Group) error {
+	groupItem := &dynamodb.PutItemInput{
+		TableName: aws.String(TABLE_NAME),
+		Item: map[string]*dynamodb.AttributeValue{
+			"groupName": {
+				S: aws.String(group.GroupName),
+			},
+			"createdBy": {
+				S: aws.String(group.CreatedBy),
+			},
+			"groupID": {
+				S: aws.String(group.GroupName),
+			},
+		},
+	}
+
+	_, err := d.databaseStore.PutItem(groupItem)
+
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func NewDynamoDBInstance() DynamoDBClient {
