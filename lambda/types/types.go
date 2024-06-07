fd875c978a32bcc4be5850b7c2941e7884ca7895
shemil <shemilkaweesha@gmail.com>
2024-06-08 00:27:22 +0530

    refactoring to types

---
+++ lambda/types/types.go
@@ -0,0 +1,18 @@
+package types
+
+import "github.com/aws/aws-lambda-go/events"
+
+type Group struct {
+	GroupName string `json:"groupName"`
+	GroupID   string `json:"groupID"`
+	CreatedBy string `json:"createdBy"`
+}
+
+type IApiEvents interface {
+	SampleRequest(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error)
+	CreateGroup(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error)
+}
+
+type IDatabase interface {
+	CreateGroup(group Group) error
+}
